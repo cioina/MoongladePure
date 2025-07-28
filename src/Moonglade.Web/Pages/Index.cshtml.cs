620e0ebe775fabb0a2b188fceb4ee6a4f03ea265
cioina <alexei_cioina@yahoo.com>
2025-07-28 07:42:09 -0700

    update all

--- src/Moonglade.Web/Pages/Index.cshtml.cs
+++ src/Moonglade.Web/Pages/Index.cshtml.cs
@@ -15,20 +15,21 @@ public class IndexModel(IBlogConfig blogConfig, IBlogCache cache, IMediator medi
     public async Task OnGet(int p = 1, string sortBy = "Recent")
     {
         var pagesize = blogConfig.ContentSettings.PostListPageSize;
-        if (Enum.TryParse(sortBy, out PostsSortBy sortByEnum))
+
+        if (!Enum.TryParse(sortBy, ignoreCase: true, out PostsSortBy sortByEnum))
         {
-            ViewData["sortBy"] = sortBy;
-
-            var posts = await mediator.Send(new ListPostsQuery(pagesize, p, sortBy: sortByEnum));
-            var totalPostsCount = await cache.GetOrCreateAsync(CacheDivision.General, "postcount", _ => mediator.Send(new CountPostQuery(CountType.Public)));
-
-            var list = new StaticPagedList<PostDigest>(posts, p, pagesize, totalPostsCount);
-
-            Posts = list;
+            sortByEnum = PostsSortBy.Recent;
+            sortBy = "Recent";
         }
-        else
-        {
-            throw new ArgumentException(message: $"Invalid argument value '{sortBy}' for argument: '{nameof(sortBy)}'!");
-        }
+
+        ViewData["sortBy"] = sortBy;
+
+        var posts = await mediator.Send(new ListPostsQuery(pagesize, p, sortBy: sortByEnum));
+        var totalPostsCount = await cache.GetOrCreateAsync(
+            CacheDivision.General,
+            "postcount",
+            _ => mediator.Send(new CountPostQuery(CountType.Public)));
+
+        Posts = new StaticPagedList<PostDigest>(posts, p, pagesize, totalPostsCount);
     }
 }
